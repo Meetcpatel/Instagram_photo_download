7901e2061abc654db1610374c0ae2fa2814161e0
Meet Patel <meetcpatel906@gmail.com>
2020-05-24 21:27:23 +0530

    new

--- insta.py
+++ insta.py
@@ -63,6 +63,7 @@ def api_id():
     # Python dictionaries to the JSON format.
     return dict
 
-port = int(os.environ.get("PORT", 5000))
+#port = int(os.environ.get("PORT", 5000))
    
-app.run(host='0.0.0.0', port=port)
+#app.run()
+app.run(host='0.0.0.0', port=80)
